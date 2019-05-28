caedb1c25e4fdc91486c79a9793c763ae5d04bc6
Shaun Hubbard <shaun@shauncodes.com>
2019-05-28 04:49:25 -0500

    setup noscript tag

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-      return try req.view().render("home", ["name": "Shaun"])
+      return try req.view().render("home", ["name": "Shaun", "title": "Welcome to Countdown Clocks"])
     }
 
     // Basic "Hello, world!" example
